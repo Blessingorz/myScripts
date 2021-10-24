13a7747d71888827021c2b2a39153024886b9ad5
wuye999 <79479594+wuye999@users.noreply.github.com>
2021-10-24 19:10:54 +0800

    Add files via upload

--- jd/jd_11Boost.py
+++ jd/jd_11Boost.py
@@ -275,12 +275,13 @@ def main():
         travel_collectAtuoScore(cookie)
         travel_getTaskDetail(cookie)
     msg(f'\n====================开始助力=========\n')
-    for e,cookie in enumerate(cookie_list,start=1):
-        for f,inviteId in enumerate(inviteId_list,start=1):
+
+    for f,inviteId in enumerate(inviteId_list,start=1):
+        for e,cookie in enumerate(cookie_list,start=1):
             if not travel_collectScore(cookie,inviteId):
                 if e!=f:
                     msg(f'账号{get_pin(cookie)}火爆或助力次数已耗尽，跳过该账号\n')
-                    break
+                    continue
     if run_send=='yes':
         send('### 双11签到加内部助力 ###')   # 通知服务
 
