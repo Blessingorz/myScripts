a98af8d3047ef1f2aeac87dbfda317262d64f424
wuye999 <79479594+wuye999@users.noreply.github.com>
2021-10-28 09:47:55 +0800

    Add files via upload

--- sendNotify.py
+++ sendNotify.py
@@ -99,20 +99,62 @@ def initialize(d):
     global notify_mode,message_info,BARK,SCKEY,TG_BOT_TOKEN,TG_USER_ID,TG_API_HOST,TG_PROXY_HOST,TG_PROXY_PORT,DD_BOT_TOKEN,DD_BOT_SECRET,Q_SKEY,QQ_MODE,QYWX_AM,PUSH_PLUS_TOKEN,PUSH_PLUS_USER
     message_info = ''''''
     notify_mode = []
-    BARK=d['BARK']                          # bark服务,自行搜索; secrets可填;
-    SCKEY=d['SCKEY']                        # Server酱的SCKEY; secrets可填
-    TG_BOT_TOKEN=d['TG_BOT_TOKEN']          # tg机器人的TG_BOT_TOKEN; secrets可填1407203283:AAG9rt-6RDaaX0HBLZQq0laNOh898iFYaRQ
-    TG_USER_ID=d['TG_USER_ID']              # tg机器人的TG_USER_ID; secrets可填 1434078534
-    TG_API_HOST=d['TG_API_HOST']            # tg 代理api
-    TG_PROXY_HOST=d['TG_PROXY_HOST']        # tg机器人的TG_PROXY_HOST; secrets可填
-    TG_PROXY_PORT=d['TG_PROXY_PORT']        # tg机器人的TG_PROXY_PORT; secrets可填
-    DD_BOT_TOKEN=d['DD_BOT_TOKEN']          # 钉钉机器人的DD_BOT_TOKEN; secrets可填
-    DD_BOT_SECRET=d['DD_BOT_SECRET']        # 钉钉机器人的DD_BOT_SECRET; secrets可填
-    QQ_SKEY=d['QQ_SKEY']                    # qq机器人的QQ_SKEY; secrets可填
-    QQ_MODE=d['QQ_MODE']                    # qq机器人的QQ_MODE; secrets可填
-    QYWX_AM=d['QYWX_AM']                    # 企业微信
-    PUSH_PLUS_TOKEN=d['PUSH_PLUS_TOKEN']    # 微信推送Plus+
-    PUSH_PLUS_USER=d['PUSH_PLUS_USER']      # plus+群组编码,可为空
+    try:
+        BARK=d['BARK']                          # bark服务,自行搜索; secrets可填;
+    except:
+        BARK=''
+    try:
+        SCKEY=d['SCKEY']                        # Server酱的SCKEY; secrets可填
+    except:
+        SCKEY=''
+    try:
+        TG_BOT_TOKEN=d['TG_BOT_TOKEN']          # tg机器人的TG_BOT_TOKEN; secrets可填1407203283:AAG9rt-6RDaaX0HBLZQq0laNOh898iFYaRQ
+    except:
+        TG_BOT_TOKEN=''
+    try:
+        TG_USER_ID=d['TG_USER_ID']              # tg机器人的TG_USER_ID; secrets可填 1434078534
+    except:
+        TG_USER_ID=''
+    try:
+        TG_API_HOST=d['TG_API_HOST']            # tg 代理api
+    except:
+        TG_API_HOST=''
+    try:
+        TG_PROXY_HOST=d['TG_PROXY_HOST']        # tg机器人的TG_PROXY_HOST; secrets可填
+    except:
+        TG_PROXY_HOST=''
+    try:
+        TG_PROXY_PORT=d['TG_PROXY_PORT']        # tg机器人的TG_PROXY_PORT; secrets可填
+    except:
+        TG_PROXY_PORT=''
+    try:
+        DD_BOT_TOKEN=d['DD_BOT_TOKEN']          # 钉钉机器人的DD_BOT_TOKEN; secrets可填
+    except:
+        DD_BOT_TOKEN=''
+    try:
+        DD_BOT_SECRET=d['DD_BOT_SECRET']        # 钉钉机器人的DD_BOT_SECRET; secrets可填
+    except:
+        DD_BOT_SECRET=''
+    try:
+        QQ_SKEY=d['QQ_SKEY']                    # qq机器人的QQ_SKEY; secrets可填
+    except:
+        QQ_SKEY=''
+    try:
+        QQ_MODE=d['QQ_MODE']                    # qq机器人的QQ_MODE; secrets可填
+    except:
+        QQ_MODE=''
+    try:
+        QYWX_AM=d['QYWX_AM']                    # 企业微信
+    except:
+        QYWX_AM=''
+    try:
+        PUSH_PLUS_TOKEN=d['PUSH_PLUS_TOKEN']    # 微信推送Plus+
+    except:
+        PUSH_PLUS_TOKEN=''
+    try:
+        PUSH_PLUS_USER=d['PUSH_PLUS_USER']      # plus+群组编码,可为空
+    except:
+        PUSH_PLUS_USER=''
 
     notify_mode = []
     message_info = ''''''
