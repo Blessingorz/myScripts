5d589e74be92603a8b0a8777c0274dd19f431449
wuye999 <79479594+wuye999@users.noreply.github.com>
2021-11-06 10:31:19 +0800

    Add files via upload

--- jd/jd_ghwzjd.py
+++ jd/jd_ghwzjd.py
@@ -3,7 +3,7 @@
 # 脚本功能为 完成任务，获得100京豆
 # 环境变量JD_COOKIE，多账号用&分割
 # export JD_COOKIE="第1个cookie&第2个cookie"
-# 11 5 17:41 修bug
+# 11 6 9:00 修bug
 
 import os,json,random,time,re,string,functools,asyncio
 import sys
@@ -204,6 +204,7 @@ def getTaskList(cookie):
                 if log:
                     if log != ' ' and log != '\n':
                         return getTaskList(cookie)
+                log=list()
         log.append(f'{get_pin(cookie)}: 全部任务已完成\n')
     else:
         log.append(f"{get_pin(cookie)}:{res['msg']}\n")
