0eb4273d5910990245709517ec3dc35cc605db83
wuye999 <wuye9999@gmail.com>
2021-12-09 20:16:19 +0800

    配置nodejs环境

--- unicom-task/docker/config.sh
+++ unicom-task/docker/config.sh
@@ -4,3 +4,8 @@
 export unicom_config_1="手机号1<<<服务密码1<<<appId1<<<抽奖次数(0-30)中奖几率渺茫<<<手机的imei（可留空）"
 export unicom_womail_1="沃邮箱登陆Url1<<<手机号1(可留空)<<<沃邮箱密码（可留空）"
 export PUSH_PLUS_TOKEN="微信推送Plus+(通知服务示例，可留空或不填)"
+
+
+
+###----------------------------------------------------------------------
+cp -rf /myScripts/unicom-task/ /root/ && cd /root/unicom-task && python3 unicom_index.py
