86d0affb46462f0352f5138684f7d4310ffe93ea
wuye999 <wuye9999@gmail.com>
2021-12-09 11:43:05 +0800

    测试

--- unicom-task/docker/docker-entrypoint.sh
+++ unicom-task/docker/docker-entrypoint.sh
@@ -8,7 +8,7 @@ fi
 
 echo -e "\n========================1. 更新脚本源代码========================\n"
 cd /myScripts
-git pull origin main
+git pull origin ${SCRIPT_BRANCH}
 echo
 
 echo -e "========================2. 检测配置文件========================\n"
