c848446bf2556f10ac1cb3c40d90f38b30cc4cf6
wuye999 <wuye9999@gmail.com>
2021-12-09 16:25:46 +0800

    config.sh

---
+++ unicom-task/docker/config.sh
@@ -0,0 +1,6 @@
+# 脚本内或环境变量填写，环境变量优先
+# 通知推送服务环境变量请查看：https://github.com/wuye999/myScripts/blob/main/send.md
+# 环境变量示例：
+export unicom_config_1="手机号1<<<服务密码1<<<appId1<<<抽奖次数(0-30)中奖几率渺茫<<<手机的imei（可留空）"
+export unicom_womail_1="沃邮箱登陆Url1<<<手机号1(可留空)<<<沃邮箱密码（可留空）"
+export PUSH_PLUS_TOKEN="微信推送Plus+(通知服务示例，可留空或不填)"
