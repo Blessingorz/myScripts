1366694800358b986538f70000b3be21f21e13d2
wuye999 <wuye9999@gmail.com>
2021-12-09 20:38:55 +0800

    .

--- unicom-task/docker/config.sh
+++ unicom-task/docker/config.sh
@@ -1,4 +1,3 @@
-# 脚本内或环境变量填写，环境变量优先
 # 通知推送服务环境变量请查看：https://github.com/wuye999/myScripts/blob/main/send.md
 # 环境变量示例：
 export unicom_config_1="手机号1<<<服务密码1<<<appId1<<<抽奖次数(0-30)中奖几率渺茫<<<手机的imei（可留空）"
